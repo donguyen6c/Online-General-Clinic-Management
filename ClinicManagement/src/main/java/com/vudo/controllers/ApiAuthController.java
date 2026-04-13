07600fb17a6c148ca060fbdacde14a31c9111cd4
donguyen6c <donguyen6c@gmail.com>
2026-04-13 12:36:14 +0700

    register api

--- ClinicManagement/src/main/java/com/vudo/controllers/ApiAuthController.java
+++ ClinicManagement/src/main/java/com/vudo/controllers/ApiAuthController.java
@@ -26,12 +26,39 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/auth")
 public class ApiAuthController {
+
     @Autowired
     private UserService userService;
-    
+
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-    
+
+    @PostMapping(path = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+    public ResponseEntity<?> register(
+            @RequestParam Map<String, String> params,
+            @RequestParam(value = "avatar", required = false) MultipartFile avatar) {
+
+        try {
+
+            if (userService.getUserByUsername(params.get("username")) != null) {
+
+                return new ResponseEntity<>("Tên đăng nhập đã tồn tại!", HttpStatus.BAD_REQUEST);
+
+            }
+
+            UserDTO newUser = userService.addUser(params, avatar);
+
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>("Lỗi hệ thống: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
         String username = credentials.get("username");
@@ -39,16 +66,16 @@ public class ApiAuthController {
 
         try {
             // 1. Dùng hàm loadUserByUsername bạn đã viết để lấy thông tin bảo mật (chứa pass băm)
-            org.springframework.security.core.userdetails.UserDetails userDetails = 
-                    userService.loadUserByUsername(username);
+            org.springframework.security.core.userdetails.UserDetails userDetails
+                    = userService.loadUserByUsername(username);
 
             // 2. So sánh mật khẩu nguyên bản với mật khẩu đã băm
             if (passwordEncoder.matches(password, userDetails.getPassword())) {
-                
+
                 // 3. Nếu đúng, lấy cục DTO sạch sẽ (không chứa pass) để trả về cho ReactJS
                 UserDTO userDTO = userService.getUserByUsername(username);
                 return new ResponseEntity<>(userDTO, HttpStatus.OK);
-                
+
             } else {
                 return new ResponseEntity<>("Sai mật khẩu!", HttpStatus.UNAUTHORIZED); // Lỗi 401
             }
